8310db8a0350d7ad492c8aad76993af5b4375803
yukito <iyggf66974@yahoo.ne.jp>
2025-02-01 11:13:53 +0900

    ok

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -19,9 +19,17 @@ class Api::V1::PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unproccessable_entity
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
   end
 
   private 
